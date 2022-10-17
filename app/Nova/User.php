16f5d99b3a0be103c2579d9cc2d54164290a79bb
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-10-17 11:33:38 +0100

    INIT

---
+++ app/Nova/User.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use Laravel\Nova\Fields\Avatar;
+use Laravel\Nova\Fields\BelongsToMany;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class User extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\User::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'name', 'email',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Avatar::make('Avatar', 'profile_photo_path')->maxWidth(50)->showWhenPeeking(),
+
+            Text::make('Name')
+                ->sortable()
+                ->rules('required', 'max:255')
+                ->showOnPreview()
+                ->showWhenPeeking(),
+
+            Text::make('Email')
+                ->sortable()
+                ->rules('required', 'email', 'max:254')
+                ->creationRules('unique:users,email')
+                ->updateRules('unique:users,email,{{ resourceId }}')
+                ->showOnPreview()
+                ->showWhenPeeking(),
+
+            Password::make('Password')
+                ->onlyOnForms()
+                ->creationRules('required', Rules\Password::defaults())
+                ->updateRules('nullable', Rules\Password::defaults())
+                ->showWhenPeeking(),
+
+            Boolean::make('Email Verified', function () {
+                return $this->hasVerifiedEmail();
+            })->showOnPreview()
+            ->showWhenPeeking(),
+
+            Boolean::make('2FA Enabled', function () {
+                return $this->hasEnabledTwoFactorAuthentication();
+            })->showOnPreview()
+                ->showWhenPeeking(),
+
+            HasMany::make('Owned Teams', 'ownedTeams', Team::class),
+
+            BelongsToMany::make('Teams', 'teams', Team::class),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(20)
+            ->withPersonalTeam()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()
+            ->withPersonalTeam()
+            ->create([
+                'name' => 'Rhys Lees',
+                'email' => 'test@example.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]);
     }
 }
